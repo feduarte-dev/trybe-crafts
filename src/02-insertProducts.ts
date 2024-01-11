d96fa8bc52b274097a9c4027cc65b73792997285
Felipe Duarte <feduarte.dev@gmail.com>
2024-01-11 13:28:16 -0300

    requisito 2 pronto

--- src/02-insertProducts.ts
+++ src/02-insertProducts.ts
@@ -1,3 +1,14 @@
-// export default function insertProducts() {
-//   // insira a solução
-// }
+export type Product = {
+  id: number,
+  name: string,
+  price: number,
+  quantity: number,
+  brands: string[]
+};
+export default function insertProducts(brands: string[], product: Product): string {
+  const checkBrands = product.brands.every((brand) => brands.includes(brand));
+  if (!checkBrands) {
+    return 'Seu produto contém marcas indisponíveis.';
+  }
+  return `${product.name} adicionado(a) com o preço de R$ ${product.price}.`;
+}
